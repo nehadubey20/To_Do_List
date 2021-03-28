a70bd749b256afe1eaf68ba8b6781027b34fc2fe
nehadubey20 <008nehadubey@gmail.com>
2021-03-28 23:40:32 +0530

    Creating packages,classes,servlets required

---
+++ src/Bean/UserBean.java
@@ -0,0 +1,52 @@
+package Bean;
+
+public class UserBean {
+
+	private int userId;
+	private String fname;
+	private String lname;
+	private String username;
+	private String password;
+	private String phone;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+
+	
+}
---
+++ src/jdbcConnector/dbConnector.java
@@ -0,0 +1,5 @@
+package jdbcConnector;
+
+public class dbConnector {
+
+}
