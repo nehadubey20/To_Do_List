6e0b38016e3a76aa961b20db226b433b1fee323f
nehadubey20 <008nehadubey@gmail.com>
2021-04-19 23:45:15 +0530

    Adding todo class

--- src/Bean/Todo.java
+++ src/Bean/Todo.java
@@ -1,39 +1,109 @@
 package Bean;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
 
-/**
- * Servlet implementation class Todo
- */
-public class Todo extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Todo() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+public class Todo {
+	
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	    private Long id;
+	    private String title;
+	    private String username;
+	    private String description;
+	    private LocalDate targetDate;
+	    private boolean status;
+
+	    protected Todo() {
+
+	    }
+
+	    public Todo(long id, String title, String username, String description, LocalDate targetDate, boolean isDone) {
+	        super();
+	        this.id = id;
+	        this.title = title;
+	        this.username = username;
+	        this.description = description;
+	        this.targetDate = targetDate;
+	        this.status = isDone;
+	    }
+
+	    public Todo(String title, String username, String description, LocalDate targetDate, boolean isDone) {
+	        super();
+	        this.title = title;
+	        this.username = username;
+	        this.description = description;
+	        this.targetDate = targetDate;
+	        this.status = isDone;
+	    }
+
+	    public Long getId() {
+	        return id;
+	    }
+
+	    public void setId(Long id) {
+	        this.id = id;
+	    }
+
+	    public String getTitle() {
+	        return title;
+	    }
+
+	    public void setTitle(String title) {
+	        this.title = title;
+	    }
+
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+
+	    public String getDescription() {
+	        return description;
+	    }
+
+	    public void setDescription(String description) {
+	        this.description = description;
+	    }
+
+	    public LocalDate getTargetDate() {
+	        return targetDate;
+	    }
+
+	    public void setTargetDate(LocalDate targetDate) {
+	        this.targetDate = targetDate;
+	    }
+
+	    public boolean getStatus() {
+	        return status;
+	    }
+
+	    public void setStatus(boolean status) {
+	        this.status = status;
+	    }
+
+	    @Override
+	    public int hashCode() {
+	        final int prime = 31;
+	        int result = 1;
+	        result = prime * result + (int)(id ^ (id >>> 32));
+	        return result;
+	    }
+
+	    @Override
+	    public boolean equals(Object obj) {
+	        if (this == obj)
+	            return true;
+	        if (obj == null)
+	            return false;
+	        if (getClass() != obj.getClass())
+	            return false;
+	        Todo other = (Todo) obj;
+	        if (id != other.id)
+	            return false;
+	        return true;
+	    }
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
-}
